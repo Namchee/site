8de52e6f2fbdcd5f5e2581cc017fdbde8b56d191
Cristopher Namchee <cristophernamchee12@gmail.com>
2022-06-16 21:39:02 +0700

    feat: add arrow animation on project showcase

--- src/components/vue/ProjectInfo.vue
+++ src/components/vue/ProjectInfo.vue
@@ -12,10 +12,8 @@ const props = defineProps<ProjectInfoProps>();
 </script>
 
 <template>
-  <AppLink
-    :href="props.link"
-    external
-    class="flex
+  <AppLink :href="props.link" external class="group
+      flex
       border-t-2 border-t-grey border-opacity-20
       transition-colors hover:bg-grey hover:bg-opacity-5
       p-6">
@@ -32,5 +30,12 @@ const props = defineProps<ProjectInfoProps>();
         {{ props.description }}
       </p>
     </div>
+
+    <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 mr-2 text-grey self-center opacity-0 translate-x-2 transition-all group-hover:opacity-100 group-hover:translate-x-0" viewBox="0 0 256 256">
+      <line x1="40" y1="128" x2="216" y2="128" fill="none" stroke="currentColor" stroke-linecap="round"
+        stroke-linejoin="round" stroke-width="16"></line>
+      <polyline points="144 56 216 128 144 200" fill="none" stroke="currentColor" stroke-linecap="round"
+        stroke-linejoin="round" stroke-width="16"></polyline>
+    </svg>
   </AppLink>
 </template>
