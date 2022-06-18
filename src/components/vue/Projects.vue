cb4efafacbba0b97a7b191e81f4727a043f114f4
Cristopher Namchee <cristophernamchee12@gmail.com>
2022-06-18 11:08:56 +0700

    feat: add proper transition for project hover

--- src/components/vue/Projects.vue
+++ src/components/vue/Projects.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
 import { ref, Ref } from 'vue';
 
-import ProjectInfo from '@/components/vue/ProjectInfo.vue';
+import AppLink from '@/components/vue/AppLink.vue';
 
-const currentProject: Ref<string> = ref('');
-
-const handleProjectHover = (project: string) => {
-  currentProject.value = project;
-}
+const currentProject: Ref<number> = ref(-1);
 
 const projects = [
   {
@@ -31,20 +27,74 @@ const projects = [
     link: 'https://fav.namchee.dev',
     image: 'https://res.cloudinary.com/namchee/image/upload/v1655466982/personal-site/Fav.webp',
   },
-]
+];
 </script>
 
 <template>
   <div class="grid grid-cols-12 gap-x-6">
     <div class="col-span-8">
-      <template v-for="project in projects" :key="project.title">
-        <ProjectInfo @hover="handleProjectHover" :year="project.year" :title="project.title"
-          :description="project.description" :link="project.link" :image="project.image" />
+      <template v-for="(project, idx) in projects" :key="`project-${idx}`">
+        <AppLink @mouseover="() => currentProject = idx" @focus="() => currentProject = idx" :href="project.link"
+          external class="group
+            flex
+            border-t-2 border-t-grey border-opacity-25
+            transition-colors hover:bg-grey hover:bg-opacity-5
+            p-6">
+          <p class="font-medium leading-none text-base tracking-tight mr-12">
+            {{ project.year }}/
+          </p>
+
+          <div class="flex-1">
+            <p class="leading-none text-xl font-medium">
+              {{ project.title }}
+            </p>
+
+            <p class="mt-4 text-grey leading-relaxed max-w-xl">
+              {{ project.description }}
+            </p>
+          </div>
+
+          <svg xmlns="http://www.w3.org/2000/svg"
+            class="w-8 h-8 mr-2 text-grey self-center opacity-0 translate-x-2 transition-all group-hover:opacity-100 group-hover:translate-x-0"
+            viewBox="0 0 256 256">
+            <line x1="40" y1="128" x2="216" y2="128" fill="none" stroke="currentColor" stroke-linecap="round"
+              stroke-linejoin="round" stroke-width="16"></line>
+            <polyline points="144 56 216 128 144 200" fill="none" stroke="currentColor" stroke-linecap="round"
+              stroke-linejoin="round" stroke-width="16"></polyline>
+          </svg>
+        </AppLink>
       </template>
     </div>
 
-    <div class="col-span-4 grid place-items-center">
-      <img :src="currentProject"  />
+    <div class="col-span-4 relative grid place-items-center">
+      <Transition v-for="(project, idx) in projects"
+        :key="`image-${idx}`"
+        name="project"
+        mode="out-in"
+      >
+        <img
+          v-if="currentProject === idx"
+          rel="preload"
+          class="w-40 h-auto absolute"
+          :src="project.image"
+          :title="project.title"
+          :alt="project.title" />
+      </Transition>
     </div>
   </div>
 </template>
+
+<style scoped>
+.project-enter-active {
+  transition: all 100ms ease-out;
+}
+
+.project-enter-from,
+.project-leave-to {
+  opacity: 0;
+}
+
+.project-enter-from {
+  transform: scale(0.95);
+}
+</style>
