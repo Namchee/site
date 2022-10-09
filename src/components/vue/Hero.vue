350f53cbde2657ac9b2912f33799f88c86dcc512
Cristopher Namchee <cristophernamchee12@gmail.com>
2022-10-09 19:07:58 +0700

    refactor: reduce y translate on project image

--- src/components/vue/Hero.vue
+++ src/components/vue/Hero.vue
@@ -1,17 +1,29 @@
 <script setup lang="ts">
-import { Box, Camera, LambertMaterial, PointLight, Renderer, Scene } from 'troisjs';
+import { Sphere, Camera, StandardMaterial, AmbientLight, Renderer, Scene } from 'troisjs';
 </script>
 
 <template>
-  <Renderer>
-    <Camera :position="{ z: 10 }" />
-    <Scene>
-      <PointLight :position="{ y: 50, z: 50 }" />
-      <Box>
-        <LambertMaterial />
-      </Box>
-    </Scene>
-  </Renderer>
+  <div class="w-full max-w-5xl mx-auto flex flex-col lg:flex-row justify-center lg:space-x-8">
+    <p class="text-2xl text-content-light tracking-tight self-center lg:text-right order-2 lg:order-1">
+      Build Softwares
+    </p>
+
+    <div class="w-full lg:w-initial min-h-[360px] order-1 self-center">
+      <Renderer antialias :alpha="true" resize="true">
+        <Camera :position="{ z: 3 }" />
+        <Scene>
+          <AmbientLight color="#EEEEEE" />
+          <Sphere :width="100" :height="100" :height-segments="64" :width-segments="32">
+            <StandardMaterial />
+          </Sphere>
+        </Scene>
+      </Renderer>
+    </div>
+
+    <p class="text-2xl text-content-dark tracking-tight self-center order-3">
+      Solve Problems
+    </p>
+  </div>
 </template>
 
 
