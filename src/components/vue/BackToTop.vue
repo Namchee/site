562ae9bba6b20dc87659ee16f43e289c4dec0677
Cristopher Namchee <cristopher@tokopedia.com>
2022-12-21 18:03:35 +0700

    style: redesign footer

---
+++ src/components/vue/BackToTop.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+interface BackToTopProps {
+  as?: string;
+  class?: string;
+}
+
+const props = withDefaults(defineProps<BackToTopProps>(), {
+  as: 'button',
+  class: ''
+})
+
+const scrollToTop = () => {
+  if (window) {
+    window.scrollTo(0, 0);
+  }
+}
+</script>
+
+<template>
+  <component
+    :is="props.as"
+    :class="props.class"
+    @click="scrollToTop"
+  >
+    <slot></slot>
+  </component>
+</template>
