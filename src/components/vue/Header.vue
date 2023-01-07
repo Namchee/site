cecff8b02e355b22e8c507ec1f67e0fc1a843738
Cristopher Namchee <cristopher@tokopedia.com>
2023-01-07 22:21:27 +0700

    feat: add logo mini interaction base code

--- src/components/vue/Header.vue
+++ src/components/vue/Header.vue
@@ -27,8 +27,21 @@ const links = [
       h-20
       flex justify-between items-center">
     <!-- start: logo -->
-    <a rel="noopener noreferrer" href="/" class="text-lg font-semibold tracking-tighter">
-      NAM•CHÉÉ
+    <a
+      rel="noopener noreferrer"
+      href="/"
+      class="text-lg leading-normal
+        font-semibold tracking-tighter
+        logo"
+    >
+      <span data-letter="N">N</span>
+      <span data-letter="A">A</span>
+      <span data-letter="M">M</span>
+      <span data-letter="•">•</span>
+      <span data-letter="C">C</span>
+      <span data-letter="H">H</span>
+      <span data-letter="É">É</span>
+      <span data-letter="É">É</span>
     </a>
     <!-- end: logo -->
 
@@ -76,5 +89,47 @@ const links = [
 
 .link:hover::before, .link:focus::before, .link:active::before {
   transform: translateY(0);
+}
+
+.logo {
+  overflow-y: hidden;
+}
+
+.logo span {
+  position: relative;
+  display: inline-block;
+}
+
+.logo span::before {
+  position: absolute;
+  content: attr(data-letter);
+  display: inline-block;
+  left: 0;
+  bottom: 0;
+  transform: translateY(100%);
+}
+
+.logo span:nth-child(1) {
+  transition: transform 300ms ease-out;
+}
+
+.logo:hover span:nth-child(1) {
+  transform: translateY(-100%);
+}
+
+.logo span:nth-child(2) {
+  transition: transform 300ms 100ms ease-out;
+}
+
+.logo:hover span:nth-child(2) {
+  transform: translateY(-100%);
+}
+
+.logo span:nth-child(3) {
+  transition: transform 300ms 200ms ease-out;
+}
+
+.logo:hover span:nth-child(3) {
+  transform: translateY(-100%);
 }
 </style>
