fd55341fb6318dd28fb76e8e0ced836073fb13f9
Cristopher Namchee <cristophernamchee12@gmail.com>
2023-12-10 21:00:33 +0700

    feat: add carbon footprint on footer

--- src/constant/easing.ts
+++
@@ -1,8 +0,0 @@
-import { Easing } from 'motion';
-
-// TODO: add more?
-export const EASING_FUNC: Record<string, Easing> = {
-  easeOutQuad: [0.5, 1, 0.89, 1],
-  easeOutCubic: [0.33, 1, 0.68, 1],
-  easeOutQuart: [0.25, 1, 0.5, 1],
-};
