627d8164557245afb7beeedaa13bd6870ac40ef5
Cristopher Namchee <cristophernamchee12@gmail.com>
2023-12-13 21:03:06 +0700

    feat: add command palette

--- src/components/vue/CommandMenu.vue
+++ src/components/vue/CommandMenu.vue
@@ -1,2 +1,145 @@
 <script setup lang="ts">
+import { ref, watch, computed } from 'vue';
+import { useMagicKeys } from '@vueuse/core';
+
+import { DialogRoot, DialogTrigger, DialogPortal, DialogOverlay, DialogContent, DialogTitle, DialogDescription } from 'radix-vue';
+
+import { Icon } from '@iconify/vue';
+
+const visible = ref(false);
+
+const keys = useMagicKeys({
+  passive: false,
+  onEventFired(e) {
+    const isCtrlK = e.ctrlKey && e.key === 'k';
+    const isMetaK = e.metaKey && e.key === 'k';
+
+    if (isCtrlK || isMetaK) {
+      e.preventDefault();
+    }
+  }
+});
+
+const ctrlK = keys['Ctrl+K'];
+const metaK = keys['Meta+K'];
+
+const links = [
+  {
+    icon: 'lucide:home',
+    name: 'Home',
+    href: '/',
+  },
+  {
+    icon: 'lucide:newspaper',
+    name: 'Posts',
+    href: '/posts',
+  },
+  {
+    icon: 'lucide:library',
+    name: 'Bookshelf',
+    href: '/bookshelf',
+  },
+  {
+    icon: 'lucide:cog',
+    name: 'Colophon',
+    href: '/colophon',
+  },
+];
+
+const searchTerm = ref('');
+
+const relevantLinks = computed(() => {
+  if (!searchTerm || !searchTerm.value) {
+    return links;
+  }
+
+  const pattern = new RegExp(searchTerm.value, 'ig');
+
+  return links.filter(link => pattern.test(link.name));
+});
+
+watch(ctrlK, (k) => {
+  if (k) {
+    visible.value = !visible.value;
+  }
+});
+
+watch(metaK, (k) => {
+  if (k) {
+    visible.value = !visible.value;
+  }
+});
 </script>
+
+<template>
+  <DialogRoot :open="visible">
+    <DialogTrigger>
+      <button
+        @click="visible = true"
+        class="grid place-items-center h-10 w-10 rounded-full border border-border fixed top-8 right-8 bg-background group focus:ring-2 focus:ring-accent focus:ring-opacity-30 focus:outline-none"
+      >
+        <span class="mt-1">⌘</span>
+        <p
+          class="absolute text-sm -bottom-8 transition-all opacity-0 translate-y-1 group-hover:opacity-100 group-hover:translate-y-0 group-focus:opacity-100 group-focus:translate-y-0"
+        >
+          Menu
+        </p>
+      </button>
+    </DialogTrigger>
+
+    <DialogPortal>
+      <DialogOverlay
+        @click="visible = false"
+        class="data-[state=open]:animate-overlayShow fixed w-screen h-screen bg-background bg-opacity-50 backdrop-blur"
+      />
+      <DialogContent class="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] border border-border rounded-md bg-background w-4/5 max-w-md shadow ">
+        <DialogTitle class="text-mauve12 m-0 text-[17px] font-semibold border-b border-border">
+          <input
+            class="w-full bg-transparent focus:outline-none text-sm p-4 placeholder:font-normal font-normal leading-relaxed"
+            placeholder="Where do you want to go next?"
+            v-model="searchTerm"
+          >
+        </DialogTitle>
+        <DialogDescription class="p-4 max-h-xs overflow-y-auto space-y-4 h-auto transition-all transition-[height] h-auto">
+          <div v-if="relevantLinks.length === 0">
+            <p class="text-sm">
+              Where's that again?
+            </p>
+          </div>
+          <div v-if="relevantLinks.length > 0">
+            <span class="font-medium text-xs mb-2">
+              Pages
+            </span>
+
+            <a
+              v-for="link in relevantLinks"
+              :key="link.href"
+              :href="link.href"
+              class="flex items-center space-x-1 p-2 text-sm transition-colors hover:bg-surface hover:text-heading focus:bg-surface focus:text-heading rounded-md"
+              rel="noopener noreferrer"
+            >
+              <Icon
+                :icon="link.icon"
+                class="w-4 h-auto mr-2"
+              />
+              <span class="relative top-[2px]">
+                {{ link.name }}
+              </span>
+            </a>
+          </div>
+        </DialogDescription>
+
+        <div class="border-t border-border py-2 px-4 text-xs">
+          Click anywhere outside the dialog or press <kbd
+            title="Command Key"
+            class="p-[2px] bg-surface font-mono text-link border border-border rounded"
+          >⌘</kbd> + <kbd class="p-[2px] bg-surface font-mono border border-border text-link rounded">K</kbd> to close the menu
+        </div>
+      </DialogContent>
+    </DialogPortal>
+  </DialogRoot>
+</template>
+
+<style>
+
+</style>
