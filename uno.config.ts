fdf83dff50acf839eec0e55db151585521d59662
Cristopher Namchee <cristophernamchee12@gmail.com>
2023-12-21 20:50:09 +0700

    refactor: remove external props and rely on protocol for _blank links

--- uno.config.ts
+++ uno.config.ts
@@ -7,15 +7,6 @@ export default defineConfig({
       sans: ['"Nacelle"', '"Roboto"', '"Arial"', 'sans-serif'],
       mono: ['JetBrains Mono'],
     },
-    fontSize: {
-      'xs': ['0.75rem', '1.5'],
-      'sm': ['0.875rem', '1.5'],
-      'base': ['1rem', '1.5'],
-      'lg': ['1.25rem', '1.5'],
-      'xl': ['1.5rem', '1.5'],
-      '2xl': ['1.875rem', '1.25'],
-      '3xl': ['2.25rem', '1.25'],
-    },
     colors: {
       background: 'oklch(var(--background))',
       surface: 'oklch(var(--surface))',
