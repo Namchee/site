a611ced4a80b0abe7561171699dfab7270305c95
Cristopher Namchee <cristophernamchee12@gmail.com>
2024-01-01 09:47:24 +0700

    chore: remove defineProps import as it's already a part of compiler macro

--- src/components/vue/CommandMenu.vue
+++ src/components/vue/CommandMenu.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch, computed, defineProps } from 'vue';
+import { ref, watch, computed } from 'vue';
 import { useMagicKeys } from '@vueuse/core';
 
 import {
