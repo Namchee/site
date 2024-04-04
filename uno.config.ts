534938caaeb2a4bf351cac7b206e47a2ad3a3888
Cristopher <cristophernamchee12@gmail.com>
2024-04-04 22:07:11 +0700

    style: use untitled typography styles

--- uno.config.ts
+++ uno.config.ts
@@ -15,11 +15,11 @@ export default defineConfig({
       mono: ['"JetBrains Mono"', 'ui-monospace'],
     },
     fontSize: {
-      xs: ['10.24px', '1.125'],
-      sm: ['12.8px', '1.5'],
+      xs: ['12px', '1.125'],
+      sm: ['14px', '1.25'],
       base: ['16px', '1.5'],
-      lg: ['20px', '1.5'],
-      xl: ['25px', '1.5'],
+      lg: ['20px', '1.75'],
+      xl: ['24px', '1.875'],
     },
     colors: {
       background: 'oklch(var(--background))',
