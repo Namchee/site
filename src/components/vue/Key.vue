f37cc72964c60e0566eafcf182688dc3fe2e4cfb
Cristopher <cristophernamchee12@gmail.com>
2024-04-17 20:43:12 +0700

    feat: add CSS class compilation step to reduce size

--- src/components/vue/Key.vue
+++ src/components/vue/Key.vue
@@ -1,6 +1,6 @@
 <template>
   <kbd
-    class="p-[2px] bg-surface font-mono text-heading border border-separator rounded-md"
+    class=":uno: p-[2px] bg-surface font-mono text-heading border border-separator rounded-md"
   >
     <slot />
   </kbd>
