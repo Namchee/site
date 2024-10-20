304a97e1a4d3dd14320d6e6afdc9efe5477a7c06
Cristopher <cristophernamchee12@gmail.com>
2024-10-20 22:05:04 +0700

    feat: add table of contents

--- src/components/vue/TableOfContents.vue
+++ src/components/vue/TableOfContents.vue
@@ -1,41 +1,66 @@
 <script setup lang="ts">
-import { DropdownMenuPortal, DropdownMenuRoot, DropdownMenuTrigger, TooltipProvider, TooltipRoot, TooltipTrigger, TooltipPortal, TooltipArrow, TooltipContent, DropdownMenuContent, DropdownMenuItem } from 'radix-vue';
+import { DropdownMenuPortal, DropdownMenuRoot, DropdownMenuTrigger, TooltipProvider, TooltipRoot, TooltipTrigger, TooltipPortal, TooltipContent, DropdownMenuContent } from 'radix-vue';
+import { onMounted, ref } from 'vue';
+
+function backToTop() {
+  if (window) {
+    window.scrollTo(0, 0);
+  }
+}
+
+const isDesktop = ref(false);
+onMounted(() => {
+  const query = window.matchMedia('(min-width: 1280px)');
+  if (query.matches) {
+    isDesktop.value = true;
+  }
+})
 </script>
 
 <template>
-  <DropdownMenuRoot :modal="false">
-    <DropdownMenuTrigger as-child>
-      <button
-        class="fixed border border-navigation-border bottom-8 z-40 bg-navigation-background md:top-32 left-8 w-8 h-8 grid place-items-center transition-colors text-content rounded-md hover:bg-navigation-accent focus:bg-navigation-accent text-sm">
-        <slot name="button" />
-      </button>
-      <!-- <TooltipProvider :delay-duration="100">
-        <TooltipRoot>
-          <div
-            class=":uno: bg-navigation-background border border-navigation-border fixed left-8 top-32 p-1 rounded-md grid place-items-center">
-            <TooltipTrigger as-child>
-
-            </TooltipTrigger>
-          </div>
-
-
+  <div
+    class=":uno: fixed border border-navigation-border bottom-8 left-8 z-40 grid place-items-center p-1 lg:p-0 lg:border-none shadow lg:shadow-none bg-navigation-background lg:top-32 lg:bottom-unset transition-colors text-content rounded-md shadow-md lg:shadow-none"
+  >
+    <TooltipProvider :delay-duration="100">
+      <TooltipRoot>
+        <DropdownMenuRoot :modal="false" :open="isDesktop" @update:open="(open) => isDesktop = open">
+          <TooltipTrigger as-child>
+            <DropdownMenuTrigger class=":uno: w-[36px] h-[36px] lg:w-auto lg:h-auto grid place-items-center transition-colors hover:bg-navigation-accent focus:bg-navigation-accent lg:hover:bg-transparent rounded-md lg:focus:bg-transparent">
+              <slot name="button" />
+            </DropdownMenuTrigger>
+          </TooltipTrigger>
           <TooltipPortal>
             <TooltipContent
-              class="text-sm rounded-md shadow py-2 tooltip__content bg-content text-background shadow-lg select-none px-3 will-change-[transform,opacity] block md:hidden"
-              :side-offset="5">
-              <p>Table of Contents</p>
-
-              <TooltipArrow :width="8" class="fill-content" />
+              side="right"
+              class="rounded-md text-sm py-2 tooltip__content bg-content text-background lg:bg-transparent lg:text-unset lg:border-none select-none px-3 will-change-[transform,opacity]"
+            >
+              Table of Contents
             </TooltipContent>
           </TooltipPortal>
-        </TooltipRoot>
-      </TooltipProvider> -->
-    </DropdownMenuTrigger>
 
-    <DropdownMenuPortal>
-      <DropdownMenuContent side="right" class=":uno: mt-4">
-        <p class=":uno: font-medium text-heading">In This Post</p>
-      </DropdownMenuContent>
-    </DropdownMenuPortal>
-  </DropdownMenuRoot>
+          <DropdownMenuPortal>
+            <DropdownMenuContent
+              align="start"
+              side="bottom"
+              @interact-outside="(e) => e.preventDefault()"
+              :force-mount="true"
+              class=":uno: lg:mt-4 lg:mb-0 mb-4 border border-navigation-border rounded-md xl:rounded-none xl:shadow-none shadow-md bg-navigation-background p-4 xl:p-0 xl:bg-transparent xl:translate-x-0 -translate-x-1 xl:border-none transition-all origin-bottom-left lg:origin-top-left data-[state=closed]:opacity-0 data-[state=opened]:opacity-100 data-[state=closed]:scale-95 data-[state=opened]:scale-100"
+            >
+              <slot name="toc" />
+
+              <button
+                class=":uno: lg:items-center space-x-2 mt-8 text-heading hidden transition-colors lg:flex text-sm"
+                @click="backToTop"
+                :tabindex="-1"
+              >
+                <slot name="btt" />
+
+                <span>Back to Top</span>
+              </button>
+            </DropdownMenuContent>
+          </DropdownMenuPortal>
+        </DropdownMenuRoot>
+      </TooltipRoot>
+    </TooltipProvider>
+  </div>
 </template>
