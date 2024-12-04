36a1da7f230e4ef499ddd41e430882b1ef4d4da2
Cristopher <cristophernamchee12@gmail.com>
2024-12-04 22:17:23 +0700

    fix: restore force-mount

--- src/components/vue/TableOfContents.vue
+++ src/components/vue/TableOfContents.vue
@@ -52,7 +52,8 @@ onMounted(() => {
                   return;
                 }
               }"
-              class=":uno: border border-navigation-border rounded-md xl:rounded-none xl:shadow-none shadow-md bg-navigation-background p-4 xl:p-0 md:mt-2 md:mb-0 mb-4 xl:mt-4 -ml-1 md:ml-0 xl:bg-transparent md:translate-x-0 xl:border-none transition-all origin-bottom-left lg:origin-top-left dropdown__menu">
+              :force-mount="true"
+              class=":uno: border border-navigation-border rounded-md xl:rounded-none xl:shadow-none shadow-md bg-navigation-background p-4 xl:p-0 md:mt-2 md:mb-0 mb-4 xl:mt-4 -ml-1 md:ml-0 xl:bg-transparent md:translate-x-0 xl:border-none transition-all origin-bottom-left lg:origin-top-left data-[state=closed]:opacity-0 data-[state=opened]:opacity-100 data-[state=closed]:scale-95 data-[state=opened]:scale-100 data-[state=closed]:pointer-events-none">
               <slot name="toc" />
 
               <button class=":uno: lg:items-center space-x-2 mt-8 text-heading hidden transition-colors md:flex text-sm"
