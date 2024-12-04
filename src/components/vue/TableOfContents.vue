877f7cf1edf43205e054221aad3a43bb3002a8ea
Cristopher <cristophernamchee12@gmail.com>
2024-12-04 22:22:52 +0700

    fix: move watcher code to vueA

--- src/components/vue/TableOfContents.vue
+++ src/components/vue/TableOfContents.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { DropdownMenuPortal, DropdownMenuRoot, DropdownMenuTrigger, TooltipProvider, TooltipRoot, TooltipTrigger, TooltipPortal, TooltipContent, TooltipArrow, DropdownMenuContent } from 'radix-vue';
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 
 function backToTop() {
   if (window) {
@@ -17,6 +17,10 @@ onMounted(() => {
     isDesktop.value = true;
     isOpen.value = true;
   }
+});
+
+watch(isOpen, () => {
+
 });
 </script>
 
@@ -52,8 +56,7 @@ onMounted(() => {
                   return;
                 }
               }"
-              :force-mount="true"
-              class=":uno: border border-navigation-border rounded-md xl:rounded-none xl:shadow-none shadow-md bg-navigation-background p-4 xl:p-0 md:mt-2 md:mb-0 mb-4 xl:mt-4 -ml-1 md:ml-0 xl:bg-transparent md:translate-x-0 xl:border-none transition-all origin-bottom-left lg:origin-top-left data-[state=closed]:opacity-0 data-[state=opened]:opacity-100 data-[state=closed]:scale-95 data-[state=opened]:scale-100 data-[state=closed]:pointer-events-none">
+              class="border border-navigation-border rounded-md xl:rounded-none xl:shadow-none shadow-md bg-navigation-background p-4 xl:p-0 md:mt-2 md:mb-0 mb-4 xl:mt-4 -ml-1 md:ml-0 xl:bg-transparent md:translate-x-0 xl:border-none dropdown__menu">
               <slot name="toc" />
 
               <button class=":uno: lg:items-center space-x-2 mt-8 text-heading hidden transition-colors md:flex text-sm"
