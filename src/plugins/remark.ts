c59afa770c209ee0eeac6c1e033e8569ffc7f0da
Cristopher <cristophernamchee12@gmail.com>
2024-12-06 22:31:29 +0700

    chore: explore remark-sectionize

--- src/plugins/remark.ts
+++ src/plugins/remark.ts
@@ -1,10 +1,12 @@
 import remarkMath from 'remark-math';
+import remarkSectionize from 'remark-sectionize';
 
 import { remarkReadingTime } from './remark/read-time';
 import { remarkLastUpdated } from './remark/last-updated';
 
 export const remarkPlugins = [
   remarkMath,
+  remarkSectionize,
   remarkReadingTime,
   remarkLastUpdated,
 ];
