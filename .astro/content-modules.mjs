3934fd37e866f78e8c45d019e4380f1fb25c57bb
Cristopher <cristophernamchee12@gmail.com>
2025-01-15 21:43:07 +0700

    blog: finish draft

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,8 +1,8 @@
 
 export default new Map([
 ["src/content/posts/embracing-code-coverage-effectively.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fembracing-code-coverage-effectively.mdx&astroContentModuleFlag=true")],
-["src/content/posts/reflections-on-my-2024.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Freflections-on-my-2024.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/hello-world.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fhello-world.mdx&astroContentModuleFlag=true")],
-["src/content/posts/utilpocalypse.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Futilpocalypse.mdx&astroContentModuleFlag=true")],
-["src/content/posts/upgrading-astro-code-snippets.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fupgrading-astro-code-snippets.mdx&astroContentModuleFlag=true")]]);
+["src/content/posts/upgrading-astro-code-snippets.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fupgrading-astro-code-snippets.mdx&astroContentModuleFlag=true")],
+["src/content/posts/reflections-on-my-2024.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Freflections-on-my-2024.mdx&astroContentModuleFlag=true")],
+["src/content/posts/utilpocalypse.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Futilpocalypse.mdx&astroContentModuleFlag=true")]]);
 		
