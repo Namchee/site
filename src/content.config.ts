aedc57fd526d82eacd998fafa8b686191abf1d57
Cristopher <cristophernamchee12@gmail.com>
2025-05-11 21:46:57 +0700

    chore: bump astro for content collection issue

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { glob } from 'astro/loaders';
 import { z, defineCollection } from 'astro:content';
 
 const posts = defineCollection({
-  loader: glob({ pattern: ['*.md'], base: 'src/content/posts' }),
+  loader: glob({ pattern: '**/*.{md,mdx}', base: 'posts' }),
   schema: z.object({
     title: z.string(),
     subtitle: z.string(),
