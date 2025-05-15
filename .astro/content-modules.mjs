f18f8c5c3c7b28c46032840385ed8f28d27f8111
Cristopher <cristophernamchee12@gmail.com>
2025-05-15 21:39:23 +0700

    blog(upgrading-astro-code-snippets): add disclaimer of not working code for 5.5.0+

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -5,6 +5,6 @@ export default new Map([
 ["posts/hello-world.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=posts%2Fhello-world.mdx&astroContentModuleFlag=true")],
 ["posts/embracing-code-coverage-effectively.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=posts%2Fembracing-code-coverage-effectively.mdx&astroContentModuleFlag=true")],
 ["posts/reflections-on-my-2024.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=posts%2Freflections-on-my-2024.mdx&astroContentModuleFlag=true")],
-["posts/utilpocalypse.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=posts%2Futilpocalypse.mdx&astroContentModuleFlag=true")],
-["posts/upgrading-astro-code-snippets.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=posts%2Fupgrading-astro-code-snippets.mdx&astroContentModuleFlag=true")]]);
+["posts/upgrading-astro-code-snippets.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=posts%2Fupgrading-astro-code-snippets.mdx&astroContentModuleFlag=true")],
+["posts/utilpocalypse.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=posts%2Futilpocalypse.mdx&astroContentModuleFlag=true")]]);
 		
