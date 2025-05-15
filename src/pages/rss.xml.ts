d5dc41890fcc5cb9f5889ca307fb950838cc4a35
Cristopher <cristophernamchee12@gmail.com>
2025-05-15 21:36:02 +0700

    chore: dependency cleanup

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,15 +1,14 @@
-import type { APIContext } from 'astro';
 import { getCollection } from 'astro:content';
 
 import rss from '@astrojs/rss';
 
-export async function GET(context: APIContext) {
+export async function GET({ site }) {
   const posts = await getCollection('posts');
 
   return rss({
     title: 'Namchee\'s Blog',
     description: 'Food for thoughts or just Namchee\'s passing whims in form of writings',
-    site: context.site,
+    site,
     items: posts.map(post => ({
       title: post.data.title,
       pubDate: post.data.publishedAt,
