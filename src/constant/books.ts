5ee5a6e3ebfe19c7b8154f17888ed0944b325680
Cristopher <cristophernamchee12@gmail.com>
2025-06-27 10:25:13 +0700

    chore: add five lines of code

---
+++ src/constant/books.ts
@@ -0,0 +1,74 @@
+// A good reason why I did this instead of using the API is because the existence of `Algorithms for Modern Hardware`
+export const BOOKS = {
+  2023: [
+    {
+      title: "The Clean Coder",
+      author: "Robert C. Martin",
+      link: "https://literal.club/book/the-clean-coder-bk300",
+    },
+    {
+      title: "Algorithms for Modern Hardware",
+      author: "Sergey Slotin",
+      link: "https://en.algorithmica.org/hpc/",
+    },
+    {
+      title: "The Pragmatic Programmer",
+      author: "Andrew Hunt & Dave Thomas",
+      link: "https://literal.club/book/the-pragmatic-programmer-f735w",
+    },
+    {
+      title: "Non Violent Communication",
+      author: "Marshall B. Rosenberg",
+      link: "https://literal.club/book/marshall-b-rosenberg-non-violent-communication-a-language-of-life-0xnv8",
+    },
+    {
+      title: "INSPIRED",
+      author: "Marthy Cagan",
+      link: "https://literal.club/book/inspired-qoqb4",
+    },
+    {
+      title: "Clean Code",
+      author: "Robert C. Martin",
+      link: "https://literal.club/book/clean-code-ewxiz",
+    },
+  ],
+  2024: [
+    {
+      title: "Cold Case Christianity",
+      author: "J. Warner Wallace",
+      link: "https://literal.club/book/j-warner-wallace-cold-case-christianity-fpe98",
+    },
+    {
+      title: "The Passionate Programmer",
+      author: "Chad Fowler",
+      link: "https://literal.club/book/the-passionate-programmer-9xgce",
+    },
+    {
+      title: "Refactoring UI",
+      author: "Adam Wathan & Steve Schoger",
+      link: "https://literal.club/book/adam-wathan-steve-schoger-refactoring-ui-8k1eh",
+    },
+    {
+      title: "System Design Interview",
+      author: "Alex Xu",
+      link: "https://literal.club/book/alex-xu-system-design-interview-an-insiders-guide-i97ig",
+    },
+    {
+      title: '100 Go Mistakes',
+      author: 'Teiva Harsanyi',
+      link: 'https://literal.club/book/teiva-harsanyi-100-go-mistakes-how-to-avoid-them-ix8kd'
+    }
+  ],
+  2025: [
+    {
+      title: "The 48 Laws of Power",
+      author: "Robert Greene",
+      link: "https://literal.club/book/the-48-laws-of-power-16lji"
+    },
+    {
+      title: 'Five Lines of Code',
+      author: 'Christian Clausen',
+      link: 'https://literal.club/book/christian-clausen-five-lines-of-code-ncxwu'
+    }
+  ]
+};
