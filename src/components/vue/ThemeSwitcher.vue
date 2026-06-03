d7aa183703a18b4cd80e55c3ad3897efd59c5b2b
Cristopher <cristophernamchee12@gmail.com>
2026-06-03 20:48:11 +0700

    chore: add LFS to library

--- src/components/vue/ThemeSwitcher.vue
+++ src/components/vue/ThemeSwitcher.vue
@@ -10,57 +10,21 @@ import TooltipContent from '@/components/vue/ui/TooltipContent.vue';
       <TooltipTrigger as-child>
         <button
           class=":uno: theme-toggle text-content m-1 p-2 rounded-md bg-transparent size-[36px] transition-colors focus:bg-surface-2 hover:bg-surface-2"
-          aria-live="polite"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-            width="1em"
-            height="1em"
-            fill="currentColor"
-            class=":uno: theme-toggle__around h-auto w-5"
-            viewBox="0 0 32 32"
-          >
+          aria-live="polite">
+          <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" width="1em" height="1em" fill="currentColor"
+            class=":uno: theme-toggle__around h-auto w-5" viewBox="0 0 32 32">
             <clipPath id="theme-toggle__around__cutout">
               <path d="M0 0h42v30a1 1 0 00-16 13H0Z" />
             </clipPath>
             <g clip-path="url(#theme-toggle__around__cutout)">
-              <circle
-                cx="16"
-                cy="16"
-                r="7"
-              />
+              <circle cx="16" cy="16" r="7" />
               <g>
-                <circle
-                  cx="16"
-                  cy="3.3"
-                  r="2.3"
-                />
-                <circle
-                  cx="27"
-                  cy="9.7"
-                  r="2.3"
-                />
-                <circle
-                  cx="27"
-                  cy="22.3"
-                  r="2.3"
-                />
-                <circle
-                  cx="16"
-                  cy="28.7"
-                  r="2.3"
-                />
-                <circle
-                  cx="5"
-                  cy="22.3"
-                  r="2.3"
-                />
-                <circle
-                  cx="5"
-                  cy="9.7"
-                  r="2.3"
-                />
+                <circle cx="16" cy="3.3" r="2.3" />
+                <circle cx="27" cy="9.7" r="2.3" />
+                <circle cx="27" cy="22.3" r="2.3" />
+                <circle cx="16" cy="28.7" r="2.3" />
+                <circle cx="5" cy="22.3" r="2.3" />
+                <circle cx="5" cy="9.7" r="2.3" />
               </g>
             </g>
           </svg>
